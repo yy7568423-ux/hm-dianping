a500699ea30e64bda4933cefb7bb6ad29f687c4a
DDDJ <2811723217@qq.com>
2026-04-03 00:26:07 +0800

    基于SortedSet重写点赞功能，实现用户根据score值排序，解决首页未登录空指针问题，mysql自动根据id排序利用ORDER BY FIELD解决

--- src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
@@ -1,8 +1,11 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.util.BooleanUtil;
+import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.hmdp.dto.Result;
+import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.Blog;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.BlogMapper;
@@ -15,7 +18,10 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.Collections;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * <p>
@@ -53,11 +59,16 @@ public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IB
 
     private void isBlogLiked(Blog blog) {
         //1、获取用户
-        Long userId = UserHolder.getUser().getId();
+        UserDTO userId = UserHolder.getUser();
+        if (userId == null) {
+            //未登录,不需要查询
+            return;
+        }
+
         //2、判断当前用户是否已经点赞
         String key = "blog:liked:" + blog.getId();
-        boolean isMember = stringRedisTemplate.opsForSet().isMember(key, userId.toString());
-        blog.setIsLike(BooleanUtil.isTrue(isMember));
+        Double score = stringRedisTemplate.opsForZSet().score(key, userId.toString());
+        blog.setIsLike(BooleanUtil.isTrue(score != null));
     }
 
     private void queryBlogUser(Blog blog) {
@@ -89,8 +100,8 @@ public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IB
         Long userId = UserHolder.getUser().getId();
         //2、判断当前用户是否已经点赞
         String key = "blog:liked:" + id;
-        boolean isMember = stringRedisTemplate.opsForSet().isMember(key, userId.toString());
-        if (BooleanUtil.isFalse(isMember)){
+        Double score = stringRedisTemplate.opsForZSet().score(key, userId.toString());
+        if (score == null){
             //3、如果未点赞，可以点赞
             //3.1、数据库点赞数+1
             boolean update = update()
@@ -98,7 +109,7 @@ public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IB
                     .eq("id", id).update();
             if (update){
                 //3.2、保存用户到Redis的Set集合
-                stringRedisTemplate.opsForSet().add(key, userId.toString());
+                stringRedisTemplate.opsForZSet().add(key, userId.toString(),System.currentTimeMillis());
             }
 
             }else{
@@ -108,8 +119,32 @@ public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IB
                     .setSql("liked = liked - 1")
                     .eq("id", id).update();
             //4.2、把用户从Redis的Set集合移除
-            stringRedisTemplate.opsForSet().remove(key, userId.toString());
+            stringRedisTemplate.opsForZSet().remove(key, userId.toString());
         }
         return Result.ok();
+    }
+
+    @Override
+    public Result queryBlogLikes(Long id) {
+        String key = "blog:liked:" + id;
+        //1、查询top5点赞用户
+        Set<String> top5 = stringRedisTemplate.opsForZSet().range(key, 0, 4);
+        if (top5 == null || top5.isEmpty()){
+            return Result.ok(Collections.emptyList());
+        }
+
+        //2、解析5个用户id
+        List<Long> ids = top5.stream().map(Long::valueOf).collect(Collectors.toList());
+        String idStr = StrUtil.join(",", ids);
+        // 3.根据用户id查询用户 WHERE id IN ( 5 , 1 ) ORDER BY FIELD(id, 5, 1)，不加order的话，结果顺序会乱
+        List<UserDTO> userDTOS = userService.query()
+                .in("id", ids)
+                .last("ORDER BY FIELD(id," + idStr + ")").list()
+                .stream()
+                .map(user -> BeanUtil.copyProperties(user, UserDTO.class))
+                .collect(Collectors.toList());
+
+        //4、返回用户信息
+        return Result.ok(userDTOS);
     }
 }
