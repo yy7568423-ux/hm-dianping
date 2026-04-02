bd3dcb35a3d73a98486ac07163b027ec63d4de01
DDDJ <2811723217@qq.com>
2026-04-02 21:53:47 +0800

    查看笔记功能

--- src/main/java/com/hmdp/config/RedissonConfig.java
+++ src/main/java/com/hmdp/config/RedissonConfig.java
@@ -19,21 +19,21 @@ public class RedissonConfig {
         config.useSingleServer().setAddress("redis://127.0.0.1:6379").setPassword("123456");
         return Redisson.create(config);
     }
-        @Bean
-        public RedissonClient redissonClient2() {
-            // 创建RedissonClient对象
-            Config config = new Config();
-            config.useSingleServer().setAddress("redis://127.0.0.1:6380");
-            return Redisson.create(config);
-        }
+//        @Bean
+//        public RedissonClient redissonClient2() {
+//            // 创建RedissonClient对象
+//            Config config = new Config();
+//            config.useSingleServer().setAddress("redis://127.0.0.1:6380");
+//            return Redisson.create(config);
+//        }
 
-            @Bean
-            public RedissonClient redissonClient3(){
-                // 创建RedissonClient对象
-                Config config = new Config();
-                config.useSingleServer().setAddress("redis://127.0.0.1:6381");
-                return Redisson.create(config);
-
-
-            }
+//            @Bean
+//            public RedissonClient redissonClient3(){
+//                // 创建RedissonClient对象
+//                Config config = new Config();
+//                config.useSingleServer().setAddress("redis://127.0.0.1:6381");
+//                return Redisson.create(config);
+//
+//
+//            }
 }
