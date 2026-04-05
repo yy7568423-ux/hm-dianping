2771879c6d5242844b877d5c2e4b532b2392088f
DDDJ <2811723217@qq.com>
2026-04-05 14:41:42 +0800

    BitMap实现签到以及签到统计功能

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -12,15 +12,19 @@ import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
 import com.hmdp.utils.RegexUtils;
+import com.hmdp.utils.UserHolder;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.connection.BitFieldSubCommands;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import static com.hmdp.utils.RedisConstants.*;
@@ -43,44 +47,33 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
 
     @Override
     public Result sendCode(String phone, HttpSession session) {
-//        1、校验手机号
+        // 1、校验手机号
         if (RegexUtils.isPhoneInvalid(phone)){
-//        2、不符合，返回错误信息
+            // 2、不符合，返回错误信息
             return Result.fail("手机号格式错误");
         }
-//        3、符合，生成验证码
+        // 3、符合，生成验证码
         String code = RandomUtil.randomNumbers(6);
-//
-//        4、保存验证码到session
-//        session.setAttribute("code", code);
 
-//        4、保存验证码到redis 设置验证码有效期5分钟
+        // 4、保存验证码到redis 设置验证码有效期5分钟
         stringRedisTemplate.opsForValue().set(LOGIN_CODE_KEY + phone, code, LOGIN_CODE_TTL, TimeUnit.MINUTES);
 
-
-//        5、发送验证码（未实现，简单记录下）
+        // 5、发送验证码（未实现，简单记录下）
         log.debug("发送短信验证码成功，验证码：{}", code);
 
-//        6、返回结果
+        // 6、返回结果
         return Result.ok();
     }
 
     @Override
     public Result login(LoginFormDTO loginForm, HttpSession session) {
-//        1、校验手机号（再次确认手机号是否正确）
+        // 1、校验手机号
         String phone = loginForm.getPhone();
         if (RegexUtils.isPhoneInvalid(phone)){
             return Result.fail("手机号格式错误");
         }
-//        2、从session校验验证码(反向校验，避免if嵌套)
-//        Object cacheCode = session.getAttribute("code");
-//        String code = loginForm.getCode();
-//        if (cacheCode == null || !cacheCode.toString().equals(code)){
-//        3、不一致，报错
-//        return Result.fail("验证码错误");
-//        }
 
-//        2、从redis校验验证码
+        // 2、从redis校验验证码
         String cacheCode = stringRedisTemplate.opsForValue().get(LOGIN_CODE_KEY + phone);
         String code = loginForm.getCode();
         if (cacheCode == null || !cacheCode.equals(code)){
@@ -88,50 +81,108 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
             return Result.fail("验证码错误");
         }
 
-
-//        4、一致，根据手机号查询用户 select * from tb_user where phone = ?
+        // 3、根据手机号查询用户
         User user = query().eq("phone", phone).one();
 
-//        5、判断用户是否存在
+        // 4、判断用户是否存在
         if (user == null){
-//        6、不存在，创建新用户并保存
-          user = createUserWithPhone(phone);
+            // 5、不存在，创建新用户
+            user = createUserWithPhone(phone);
         }
-//        7、保存用户信息到session
-//        session.setAttribute("user", BeanUtil.copyProperties(user, UserDTO.class));
 
-
-//        7、保存用户信息到redis
-//          1、生成token，作为登录令牌
+        // 6、保存用户信息到redis
+        // 生成token
         String token = UUID.randomUUID().toString(true);
         UserDTO userDTO = BeanUtil.copyProperties(user, UserDTO.class);
-//          2、将User对象转为HashMap存储
+
+        // 转为Map存储
         Map<String, Object> userMap = BeanUtil.beanToMap(userDTO, new HashMap<>(),
-              CopyOptions.create()
-                   .setIgnoreNullValue(true)
-                   .setFieldValueEditor((fieldName, fieldValue) -> fieldValue.toString()));
-//          3、将HashMap存储到redis中
+                CopyOptions.create()
+                        .setIgnoreNullValue(true)
+                        .setFieldValueEditor((fieldName, fieldValue) -> fieldValue.toString()));
+
+        // 存入Redis
         String tokenKey = LOGIN_USER_KEY + token;
         stringRedisTemplate.opsForHash().putAll(tokenKey, userMap);
-//          4、设置token有效期
         stringRedisTemplate.expire(tokenKey, LOGIN_USER_TTL, TimeUnit.MINUTES);
 
-
-
-//          8、返回token
+        // 返回token
         return Result.ok(token);
+    }
+
+    @Override
+    public Result sign() {
+        // 1.获取当前登录用户
+        Long userId = UserHolder.getUser().getId();
+        // 2.获取日期
+        LocalDateTime now = LocalDateTime.now();
+        // 3.拼接key
+        String keySuffix = now.format(DateTimeFormatter.ofPattern("yyyyMM"));
+        String key = "sign:" + userId + ":" + keySuffix;
+        // 4.今天是本月第几天
+        int dayOfMonth = now.getDayOfMonth();
+        // 5.写入Redis
+        stringRedisTemplate.opsForValue().setBit(key, dayOfMonth - 1, true);
+        return Result.ok();
+    }
+
+    @Override
+    public Result signCount() {
+        //获取本月到今天为止的所有签到记录
+        // 1.获取当前登录用户
+        Long userId = UserHolder.getUser().getId();
+        // 2.获取日期
+        LocalDateTime now = LocalDateTime.now();
+        // 3.拼接key
+        String keySuffix = now.format(DateTimeFormatter.ofPattern("yyyyMM"));
+        String key = "sign:" + userId + ":" + keySuffix;
+        // 4.今天是本月第几天
+        int dayOfMonth = now.getDayOfMonth();
+        // 5.获取本月到今天为止的所有签到记录
+        List<Long> result = stringRedisTemplate.opsForValue().bitField(key, BitFieldSubCommands.create().get(BitFieldSubCommands.BitFieldType.unsigned(dayOfMonth)).valueAt(0));
+        if (result == null || result.size() == 0) {
+            //未签到
+            return Result.ok(0);
+        }
+        Long number = result.get(0);
+        if (number == 0|| number == null){
+            return Result.ok(0);
+        }
+        //循环遍历
+        int count = 0;
+        while (true){
+            //让这个数字与1做与运算，得到数字的最后一个bit位 判断这个bit位是否为0
+            if ((number & 1) == 0) {
+                //如果为0，说明未签到，结束
+                break;
+            }else {
+                //如果为1，说明已签到，计数器+1
+                count++;
+            }
+            //移出最右边的bit位 继续下一个bit位
+//            number = number >> 1;
+              number >>>= 1;
+        }
+        return Result.ok(count);
 
 
-//        return Result.ok();
+
+
+
+
+
+
+
+
+
     }
 
     private User createUserWithPhone(String phone) {
         User user = new User();
         user.setPhone(phone);
         user.setNickName(USER_NICK_NAME_PREFIX + RandomUtil.randomString(10));
-        //保存用户
+        // 保存用户
         save(user);
         return user;
-
     }
 }
